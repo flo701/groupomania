55162ed430d626824fbd9537d6f827bdab6452af
Florence Leclercq <leclercqflorence122@orange.fr>
2022-09-29 22:29:49 +0200

    Modification de la page connexion

--- front-end/src/components/ConnectionForm/index.jsx
+++ front-end/src/components/ConnectionForm/index.jsx
@@ -19,28 +19,64 @@ const ConnectionForm = (props) => {
     }
   }
 
+  const token = getCookie('token')
+
   return (
-    <div className="connection-form">
-      <ul>
-        <li
-          onClick={handleModals}
-          id="connection-form_register"
-          className={signUpModal ? 'connection-form_active-btn' : null}
-        >
-          S'inscrire
-        </li>
-        <li
-          onClick={handleModals}
-          id="connection-form_login"
-          className={signInModal ? 'connection-form_active-btn' : null}
-        >
-          Se connecter
-        </li>
-      </ul>
-      {signUpModal && <SignUpForm />}
-      {signInModal && <SignInForm />}
-    </div>
+    <>
+      {token ? (
+        <>
+          <h3 className="posts_h3">{''} </h3>
+        </>
+      ) : (
+        <>
+          <h3 className="posts_h3">
+            Bienvenue sur Groupomania, votre réseau social d'entreprise
+          </h3>
+          <p className="posts_p">
+            Veuillez vous connecter pour créer un post et consulter les posts de
+            vos collègues
+          </p>
+        </>
+      )}
+      <div className="connection-form">
+        <ul>
+          <li
+            onClick={handleModals}
+            id="connection-form_register"
+            className={signUpModal ? 'connection-form_active-btn' : null}
+          >
+            S'inscrire
+          </li>
+          <li
+            onClick={handleModals}
+            id="connection-form_login"
+            className={signInModal ? 'connection-form_active-btn' : null}
+          >
+            Se connecter
+          </li>
+        </ul>
+        {signUpModal && <SignUpForm />}
+        {signInModal && <SignInForm />}
+      </div>
+    </>
   )
+}
+
+// https://www.w3schools.com/js/js_cookies.asp :
+function getCookie(cname) {
+  let name = cname + '='
+  let decodedCookie = decodeURIComponent(document.cookie)
+  let ca = decodedCookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
 }
 
 export default ConnectionForm
