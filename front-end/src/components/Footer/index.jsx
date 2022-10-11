0de33c71721ce2ac6af3c71db5504ba8c1ca2677
Florence Leclercq <leclercqflorence122@orange.fr>
2022-10-11 17:47:06 +0200

    Ajout d'un tooltip dans Footer

--- front-end/src/components/Footer/index.jsx
+++ front-end/src/components/Footer/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import GroupomaniaLogo from '../../assets/logo/icon-left-font.png'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css'
 
 const Footer = () => {
   return (
     <div className="footer_img-container">
       <a href="/">
-        <img src={GroupomaniaLogo} alt="logo groupomania" />
+        <Tippy content="Revenir en haut de la page d'accueil">
+          <img src={GroupomaniaLogo} alt="logo groupomania" />
+        </Tippy>
       </a>
     </div>
   )
