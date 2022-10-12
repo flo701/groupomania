851bd4d9084d7438cd5f7bb1362263d895538f83
Florence Leclercq <leclercqflorence122@orange.fr>
2022-10-12 22:03:14 +0200

    Nettoyage dans UserProfile/index.jsx

--- front-end/src/pages/UserProfile/index.jsx
+++ front-end/src/pages/UserProfile/index.jsx
@@ -1,55 +1,10 @@
 import React from 'react'
 import UserInfos from '../../components/UserInfos/index.jsx'
-// import axios from 'axios'
-// import { timestampParser } from '../../components/Utils'
-// import { useState } from 'react'
 
 const UserProfile = () => {
-  //   const params = new URLSearchParams(window.location.search)
-  //   const userId = params.get('id')
-
-  //   const token = getCookie('token')
-  //   const headers = { Authorization: `Bearer ${token}` }
-
-  //   const [profileImage, setProfileImage] = useState('')
-  //   const [firstname, setFirstname] = useState('')
-  //   const [lastname, setLastname] = useState('')
-  //   const [creationDate, setCreationDate] = useState('')
-
-  //   const [isActive, setIsActive] = useState()
-
-  //   const getOneUser = (id) => {
-  //     axios({
-  //       method: 'get',
-  //       url: `${process.env.REACT_APP_API_URL}/api/auth/getOneUser/${id}`,
-  //       headers: headers,
-  //     })
-  //       .then((res) => {
-  //         if (res.data[0].active === 1) {
-  //           setIsActive(true)
-  //         } else {
-  //           setIsActive(false)
-  //         }
-  //         setProfileImage(res.data[0].profileImage)
-  //         setFirstname(res.data[0].firstname)
-  //         setLastname(res.data[0].lastname)
-  //         setCreationDate(timestampParser(res.data[0].creationDate))
-  //       })
-  //       .catch((err) => {
-  //         console.log(err)
-  //       })
-  //   }
-  //   getOneUser(userId)
-
   return (
     <div>
-      <UserInfos
-      // profileImage={profileImage}
-      // firstname={firstname}
-      // lastname={lastname}
-      // creationDate={creationDate}
-      // isActive={isActive}
-      />
+      <UserInfos />
     </div>
   )
 }
