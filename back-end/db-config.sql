a018c6738577866d524769424019f219b89a63a3
Florence Leclercq <leclercqflorence122@orange.fr>
2022-10-13 13:01:47 +0200

    Modification de db-config.sql

--- back-end/db-config.sql
+++ back-end/db-config.sql
@@ -33,7 +33,7 @@ CREATE TABLE `likes` (
   KEY `post_id` (`post_id`),
   CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=1520 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1564 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `likes` (
 
 LOCK TABLES `likes` WRITE;
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
-INSERT INTO `likes` VALUES (1275,195,279),(1423,195,315),(1425,194,138),(1427,194,331),(1433,178,331),(1436,178,291),(1446,196,138),(1464,196,456),(1473,196,292),(1512,265,291),(1518,265,292);
+INSERT INTO `likes` VALUES (1275,195,279),(1423,195,315),(1425,194,138),(1427,194,331),(1433,178,331),(1436,178,291),(1446,196,138),(1464,196,456),(1473,196,292),(1518,265,292),(1556,265,291),(1562,265,331);
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +64,7 @@ CREATE TABLE `post` (
   PRIMARY KEY (`id`),
   KEY `post_ibfk_1` (`user_id`),
   CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=611 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=622 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +73,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (138,'On a (encore) le droit de rêver...','Alors profitons-en ! Toutefois, je trouve les habitations un peu trop sages et alignées. Chacune mériterait de se différencier des autres... comme les êtres humains. Cette photo m\'inspire, tout est relié. Il fallait bien écrire 255 caractères. C\'est fait.','2022-10-01 14:35:43','http://localhost:3000/images/Ile_paradisiaque1664627743032.jpg',178,2),(279,'Titre avec 40 caractères. Voilà voilà...','Description avec 255 caractères. Pour l\'instant, je n\'ai rien de spécial à dire. Alors je ne parle pas pour ne rien dire. Quand j\'aurai quelque chose à dire, je vous le dirai. Wouaaah ! Les répétitions ! Mais c\'était juste pour avoir quelque chose à dire.','2022-09-25 10:10:58',NULL,178,1),(291,'Quelle beauté...','On a l\'impression qu\'il nous sourit ;-)','2022-09-27 13:25:51','http://localhost:3000/images/dauphin1664276986631.jpg',194,2),(292,'Nos amis les animaux','Aimons-les comme ils nous aiment. Ils sont incroyables...','2022-09-29 13:41:07','http://localhost:3000/images/animaux1664451667893.jpg',195,2),(315,'Qui me suit ?','Je pars profiter un peu de la vie, et prendre soin de moi...','2022-10-07 22:15:37','http://localhost:3000/images/bora-bora21665173737796.jpg',196,1),(331,'Allez ! Plouf !','Plongée dans un autre univers... Et regardez-moi ces couleurs ! C\'est incroyable...','2022-10-08 09:19:17','http://localhost:3000/images/poissons1665174205372.jpg',230,2),(456,'Incroyable beauté','La nature est magnifique','2022-10-07 11:55:46','http://localhost:3000/images/arbre-a-fleurs1665136546776.webp',265,1);
+INSERT INTO `post` VALUES (138,'On a (encore) le droit de rêver...','Alors profitons-en ! Toutefois, je trouve les habitations un peu trop sages et alignées. Chacune mériterait de se différencier des autres... comme les êtres humains. Cette photo m\'inspire, tout est relié. Il fallait bien écrire 255 caractères. C\'est fait.','2022-10-01 14:35:43','http://localhost:3000/images/Ile_paradisiaque1664627743032.jpg',178,2),(279,'Titre avec 40 caractères. Voilà voilà...','Description avec 255 caractères. Pour l\'instant, je n\'ai rien de spécial à dire. Alors je ne parle pas pour ne rien dire. Quand j\'aurai quelque chose à dire, je vous le dirai. Wouaaah ! Les répétitions ! Mais c\'était juste pour avoir quelque chose à dire.','2022-10-13 11:42:24',NULL,178,1),(291,'Quelle beauté...','On a l\'impression qu\'il nous sourit ;-)','2022-09-27 13:25:51','http://localhost:3000/images/dauphin1664276986631.jpg',194,2),(292,'Nos amis les animaux','Aimons-les comme ils nous aiment. Ils sont incroyables...','2022-09-29 13:41:07','http://localhost:3000/images/animaux1664451667893.jpg',195,2),(315,'Qui me suit ?','Je pars profiter un peu de la vie, et prendre soin de moi...','2022-10-07 22:15:37','http://localhost:3000/images/bora-bora21665173737796.jpg',196,1),(331,'Plongée dans un autre univers','J\'ai goûté à une sérénité indescriptible lors de l\'immersion dans cet autre monde... \r\nEt ces couleurs ! C\'est incroyable...','2022-10-13 11:42:59','http://localhost:3000/images/poissons1665174205372.jpg',230,3),(456,'Incroyable beauté','La nature est magnifique !','2022-10-13 11:42:14','http://localhost:3000/images/arbre-a-fleurs1665136546776.webp',265,1);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `user` (
   `profileImage` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=302 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=306 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +105,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (178,'USER',1,'Redancourt','Jean-Christophe','jean@gmail.com','$2b$10$RBLGmF2n2mdoQC1oDk4itOxjONr3uYefSsu5iyxvpw3FqsTPBkwxy','2022-09-17 18:28:56','http://localhost:3000/images/jean-christophe1664548458548.webp'),(194,'USER',1,'Duchemin','Sophie','soso@hello.com','$2b$10$WCeaG7z4/W2mrWQDZRzzW.DxzhpgxQf4h2DxQG10JCRnUsL0IM3Kq','2022-09-25 20:27:06','http://localhost:3000/images/sophie1664548311449.webp'),(195,'USER',1,'Durand','Lola','lola@yahoo.fr','$2b$10$nOvqVkRCUrVQyd7oEkcOsOpvW0cUnON.bMKTZC4Uq8GRdRleUjBNG','2022-09-26 12:26:00','http://localhost:3000/images/lola1664548356413.webp'),(196,'USER',1,'Lefort','Guy','guy@orange.fr','$2b$10$Z.I6dajgWBySeDY8ns1BL.UGVZLUDnSI79kB7LUw4zRgMi7ko0VsK','2022-09-26 18:07:32','http://localhost:3000/images/guy1664704133834.webp'),(230,'USER',1,'Lun','Gérard ','gege@gmail.com','$2b$10$jASVJLABSc/0nKH0F2m2e.UipWTwfJ84/iCzKDSwC.9RQSXS5WRfe','2022-10-02 21:57:40','http://localhost:3000/images/gerard1664788655829.jpg'),(265,'ADMIN',1,'Real','Louise','louise@gmail.com','$2b$10$DzdCTuUgIu0hVBWcymqkQec0BrMJslD4r.mIJyrOHyCP9J3eOL.SK','2022-10-07 11:49:27','http://localhost:3000/images/louise1665426464083.webp');
+INSERT INTO `user` VALUES (178,'USER',1,'Redancourt','Jean-Christophe','jean@gmail.com','$2b$10$RBLGmF2n2mdoQC1oDk4itOxjONr3uYefSsu5iyxvpw3FqsTPBkwxy','2022-09-17 18:28:56','http://localhost:3000/images/jean-christophe1664548458548.webp'),(194,'USER',1,'Duchemin','Sophie','soso@hello.com','$2b$10$WCeaG7z4/W2mrWQDZRzzW.DxzhpgxQf4h2DxQG10JCRnUsL0IM3Kq','2022-09-25 20:27:06','http://localhost:3000/images/sophie1664548311449.webp'),(195,'USER',1,'Durand','Lola','lola@yahoo.fr','$2b$10$nOvqVkRCUrVQyd7oEkcOsOpvW0cUnON.bMKTZC4Uq8GRdRleUjBNG','2022-09-26 12:26:00','http://localhost:3000/images/lola1664548356413.webp'),(196,'USER',1,'Lefort','Guy','guy@orange.fr','$2b$10$Z.I6dajgWBySeDY8ns1BL.UGVZLUDnSI79kB7LUw4zRgMi7ko0VsK','2022-09-26 18:07:32','http://localhost:3000/images/guy1664704133834.webp'),(230,'USER',1,'Lun','Gérard ','gege@gmail.com','$2b$10$IikzWhqHOwle2B8kdTE4deIBjLPp5VYqgvQ5/wgpjEhR/tYbDnaJq','2022-10-02 21:57:40','http://localhost:3000/images/gerard1664788655829.jpg'),(265,'ADMIN',1,'Real','Louise','louise@gmail.com','$2b$10$8UzevRPmS0fcQQ.uZu18nuPd1iyiSmSIylXHGP1B8f2pO8rDCeYG2','2022-10-07 11:49:27','http://localhost:3000/images/louise1665426464083.webp');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -118,4 +118,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-10 22:37:28
+-- Dump completed on 2022-10-13 12:57:31
